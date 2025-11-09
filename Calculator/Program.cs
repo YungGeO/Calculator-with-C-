b499b524b4108a25276a165a56d63f9c62042f10
George Zacharias <zachariasg7@hotmail.gr>
2025-11-09 18:56:21 +0200

    setting up the while loop

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,15 @@
 ﻿using System;
-
-Console.WriteLine("What is your name kind sir");
-string name = Console.ReadLine();
-Console.WriteLine($"welcome {name}");
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your name kind sir");
+        string name = Console.ReadLine();
+        Console.WriteLine($"welcome {name}");
+        bool stopCal = true;
+        while (stopCal)
+        {
+            Console.WriteLine($"What you want to do mr/mrs {name}");
+        }
+    }
+}
